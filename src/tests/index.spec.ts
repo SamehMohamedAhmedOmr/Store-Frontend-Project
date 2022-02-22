d6cf5c4f570b27245d2c037aea4313edd53c6fc0
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2022-02-22 09:15:05 +0200

    Update index.spec.ts

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -3,11 +3,11 @@ import app from '../server';
 
 const request = supertest(app);
 
-describe('suite description', () => {
-  it('Test API RESPONSE TO 200', async () => {
-    const response = await request.get(
-      '/api/users?image=image-4.jpg&width=1000&height=500'
-    );
-    expect(response.status).toBe(200);
-  });
-});
+// describe('suite description', () => {
+//   it('Test API RESPONSE TO 200', async () => {
+//     const response = await request.get(
+//       '/api/users?image=image-4.jpg&width=1000&height=500'
+//     );
+//     expect(response.status).toBe(200);
+//   });
+// });
